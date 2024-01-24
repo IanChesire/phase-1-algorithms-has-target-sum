e19131cd2f89352f319f28a3f6eb123c3e2e5652
Ian <iankosgey535@gmail.com>
2024-01-24 16:43:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+   const arrayOfNumbers = new Set();
+   for (let num of array) {
+     const number = target - num;
+     if (arrayOfNumbers.has(number)) {
+       return true;
+     }
+     arrayOfNumbers.add(num);
+   }
+   return false;
+ }
 
 /* 
   Write the Big O time complexity of your function here
+
+  *The function iterates through each element of the array exactly once.
+  *For each element, the function performs constant time operations that do not depend on the size of the array
+   *The loop runs in linear time, making the overall time complexity O(n).
 */
 
 /* 
   Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
+  START
+  *Input-array
+        -target
+  *Output-true if any pair of numbers in the array adds upto the target
+        -otherwise is false
+
+  *Initialize an empty set called 'arrayOfNumbers' to store number arrays
+  *Iterate through each number in the array
+  *Return false if loop completes without finding a pair
+  END
 */
 
 // You can run `node index.js` to view these console logs
